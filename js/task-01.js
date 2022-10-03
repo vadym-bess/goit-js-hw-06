a7db616a3ac47c964d8f30fe8dfd27562c17baef
Vadym <besarabvadim2@gmail.com>
2022-10-03 17:21:39 +0300

    ksjdbkjrghksj

--- js/task-01.js
+++ js/task-01.js
@@ -1,1 +1,20 @@
-const 
+const categories = document.querySelectorAll(".item");
+console.log(`Number of categories: ${categories.length}`);
+
+categories.forEach(category => {
+const groups = [...category.children];
+
+    groups.forEach(group => {
+        if (group.tagName === 'H2') {
+            console.log(`Category: ${group.textContent}`)
+        }
+    })
+
+    groups.forEach(group => {
+        if (group.tagName === 'UL') {
+            console.log(`Elements: ${group.children.length}`);
+        }
+            
+    })
+});
+   
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+
+
+
+
+const navRef = document.querySelector('#ingredients');
+
+const listTotal = ingredients.map(ingredient => {
+let itemRef = document.createElement("li");
+itemRef.textContent = ingredient;
+itemRef.classList.add("item");
+return itemRef
+});
+
+navRef.append(...listTotal)
