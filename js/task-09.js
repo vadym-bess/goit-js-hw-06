a3826cb81bb77eee6a5138ac5707e46b8841dd8d
Vadym <besarabvadim2@gmail.com>
2022-10-08 11:38:19 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const refs = {
+  buttonRef: document.querySelector(".change-color"),
+  spanRef: document.querySelector(".color"),
+};
+
+refs.buttonRef.addEventListener("click", onChangeColor);
+
+function onChangeColor(event) {
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+  refs.spanRef.textContent = color;
 }
